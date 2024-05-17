f6974e930f689337bf0ae7378dcad793ebb174a2
Juan11211 <avilesjuan628@gmail.com>
2024-05-17 14:44:45 -0400

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import * as React from 'react'
-import {BrowserRouter, Routes } from 'react-router-dom';
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
 import { routes } from './routes/index'
+import LandingPage from './components/LandingPage/LandingPage.Component'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <React.Fragment>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<LandingPage />} />
           {routes}
         </Routes>
       </BrowserRouter>
---
+++ src/redux/store.ts
@@ -0,0 +1,15 @@
+import  { configureStore } from "@reduxjs/toolkit";
+import {combineReducers } from 'redux';
+
+
+const reducer = combineReducers({
+    
+})
+
+export const store = configureStore({
+    reducer: reducer
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
